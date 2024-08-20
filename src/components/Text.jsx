3ceb7a2a73cdc8de7feb8fbb945a9e788ea75868
Kim <kimettahri8@gmail.com>
2024-08-21 01:40:38 +0200

    first commit

---
+++ src/components/Text.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Text() {
+  return (
+    <div className="absolute flex flex-col text-[5.5vw] uppercase w-[80vw] items-start leading-tight">
+      <p>Hakim Ettahri</p>
+      <p className="self-end">Front End Developer</p>
+      <p>Based in France</p>
+      <p className="self-end">Portfolio@2024</p>
+    </div>
+  )
+}
